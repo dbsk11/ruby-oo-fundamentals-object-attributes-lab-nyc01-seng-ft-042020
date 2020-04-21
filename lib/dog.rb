34b54b5d4a7ad713a1baaf6406feb3e25a32578f
Jessica <github email address>
2020-04-21 16:32:30 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,20 +6,19 @@ class Dog
         @breed = breed
     end
     
-    def name=(new_name)
-        @name = new_name
-    end
-
     def name
         @name
     end
-
-    def breed=(new_breed)
-        @breed = new_breed
+    
+    def name=(new_name)
+        @name = new_name
     end
 
     def breed
         @breed
     end
 
+    def breed=(new_breed)
+        @breed = new_breed
+    end
 end
